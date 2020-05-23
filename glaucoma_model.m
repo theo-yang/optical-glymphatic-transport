788307512cb494db217303c16c3f1209e70831b2
theo-yang <tsyang@caltech.edu>
2020-05-22 19:52:35 -0700

    simulations for p and c driven glaucoma

--- glaucoma_model.m
+++ glaucoma_model.m
@@ -1,45 +1,99 @@
 %% Glymphatic Clearance
-%% Solve BVP
+%% Load Data
 clc
 close all
 clear all
 
-% Set Peclet and Sherwood Numbers
-Pe = 2;
+% load data
+[~,sheet_name]=xlsfinfo('data.xlsx');
+for k=1:numel(sheet_name)
+  data{k}=xlsread('data.xlsx',sheet_name{k});
+end
+%% Solve BVP for changes in TPD
+
+%--------------------Calucate average TPD in Fig 3A------------------------
+% Get Data From Fig 3B
+i2a = find(contains(sheet_name,'Fig2A'));
+f2a = data{i2a};
+IOP_mean = 18;
+ICP = [16.7,3.6,0.5];
+TLD = IOP_mean - ICP;
+
+% -----------------------------Solve BVP------------------------------------
+% Define Normal as Pe = 2, Sh = 0.5
+Pe = 2 .* TLD ./ TLD(2);
 Sh = 0.5;
 
-% Solve non-dimensional elliptical problem
-r = linspace(0,1);
-z = linspace(0,1);
-pde = @(r,z,w,dwdr) pdefun(r,z,w,dwdr,Pe);
-radial_bc = @(r0,w0,rR,wR,z) rbc(r0,w0,rR,wR,z,Sh);
-sol = pdepe(1,pde,@zbc,radial_bc,r,z);
+% define r and z mesh
+r = linspace(0,100);
+z = linspace(0,100);
 
-% Plot r by z heatmap
-close all
-r_by_z = [flip(sol',1);sol']; 
+% Solve and Plot r by z heatmaps
 f = figure;
-hold on
-width=1000;
-height=200;
+% width=1000;
+% height=200;
 rspan = linspace(0,500);
 zspan = linspace(0,5000);
-imagesc(zspan,rspan,r_by_z)
-set(gcf,'position',[10,10,width,height])
-title(sprintf('High IOP: Pe = %-5.1f,  Sh = %-5.1f',Pe,Sh),'fontsize',16)
-xlabel('z (\mum)','fontsize',16)
-ylabel('r (\mum)','fontsize',16)
-ylim([min(rspan) max(rspan)])
-xlim([min(zspan) max(zspan)])
-cbar = colorbar;
-caxis([0 1])
-yl = ylabel(cbar,'$\frac{C_A}{C_{A0}}$','Interpreter','latex');
-set(yl,'fontsize',20)
-set(yl,'rotation',0)
-pos = get(yl,'Position');
-cbar.Label.Position = [pos(1)+1 pos(2) + 0.1];
-hold off
-% saveas(f,'highIOP_eye.jpg')
+for i = 1:3
+    sol = solve_BVP(Pe(i),Sh,.7);
+    r_by_z = [flip(sol',1);sol']; 
+    subplot(3,1,i);
+    hold on
+    imagesc(zspan,rspan,r_by_z)
+    %set(gcf,'position',[10,10,width,height])
+    title(sprintf('Pe = %-5.2f,  Sh = %-5.1f',Pe(i),Sh),'fontsize',16)
+    xlabel('z (\mum)','fontsize',14)
+    ylabel('r (\mum)','fontsize',14)
+    ylim([min(rspan) max(rspan)])
+    xlim([min(zspan) max(zspan)])
+    cbar = colorbar;
+    caxis([0 1])
+    yl = ylabel(cbar,'$\frac{C_A}{C_{A0}}$','Interpreter','latex');
+    set(yl,'fontsize',18)
+    set(yl,'rotation',0)
+    pos = get(yl,'Position');
+    cbar.Label.Position = [pos(1) + 1 pos(2)+0.3];
+    hold off
+    
+end
+%% Solve BVP for changes in lamina cibrosa permeability
+
+% Define Normal as Pe = 2, Sh = 0.5
+Pe = [0.5,2,2];
+Sh = [0.5,0.5,0.2];
+
+% define r and z mesh
+r = linspace(0,100);
+z = linspace(0,100);
+
+% Solve and Plot r by z heatmaps
+f = figure;
+% width=1000;
+% height=200;
+rspan = linspace(0,500);
+zspan = linspace(0,5000);
+for i = 1:3
+    sol = solve_BVP(Pe(i),Sh(i),1);
+    r_by_z = [flip(sol',1);sol']; 
+    subplot(3,1,i);
+    hold on
+    imagesc(zspan,rspan,r_by_z)
+    %set(gcf,'position',[10,10,width,height])
+    title(sprintf('Pe = %-5.2f,  Sh = %-5.1f',Pe(i),Sh(i)),'fontsize',16)
+    xlabel('z (\mum)','fontsize',14)
+    ylabel('r (\mum)','fontsize',14)
+    ylim([min(rspan) max(rspan)])
+    xlim([min(zspan) max(zspan)])
+    cbar = colorbar;
+    caxis([0 1])
+    yl = ylabel(cbar,'$\frac{C_A}{C_{A0}}$','Interpreter','latex');
+    set(yl,'fontsize',18)
+    set(yl,'rotation',0)
+    pos = get(yl,'Position');
+    cbar.Label.Position = [pos(1) + 1 pos(2)+0.3];
+    hold off
+    
+end
 %% Create Movie 
 close all
 
@@ -144,9 +198,9 @@ function [c,f,s] = pdefun(r,z,w,dwdr,Pe)
     f = dwdr;
     s = 1 ./ r .* dwdr;
 end
-function w0 = zbc(z)
+function w0 = zbc(z,lb)
 % Define axial Boundary Condition for glymphatic clearance model 
-    w0 = 1;
+    w0 = lb;
 end
 function [p0,q0,pR,qR] = rbc(r0,w0,rR,wR,z,Sh)
 % Define radial Boundary Conditions for glymphatic clearance model 
@@ -162,4 +216,15 @@ xunit = 1 * cos(th);
 yunit = 1 * sin(th);
 zunit = zeros(1,length(xunit)) + z;
 h = plot3(xunit,yunit,zunit,'red','Parent',ax);
+end
+
+function sol = solve_BVP(Pe,Sh,lb)
+% Solves the PDE for given Peclet and Sherwood numbers, and plots an r_by_z
+% heatmap with supplied title
+% If save is true, saves the heatmap as 'title.jpg'
+r = linspace(0,1);
+z = linspace(0,1);
+pde = @(r,z,w,dwdr) pdefun(r,z,w,dwdr,Pe);
+radial_bc = @(r0,w0,rR,wR,z) rbc(r0,w0,rR,wR,z,Sh);
+sol = pdepe(1,pde,@(z) zbc(z,lb),radial_bc,r,z);
 end
